abcb3b2b60d51c143723f6d676018a041d0641e9
spectator8 <86881057+spectator8@users.noreply.github.com>
2023-03-19 23:20:41 +0100

    Update checkVehicleByVIN.ps1

--- checkVehicleByVIN.ps1
+++ checkVehicleByVIN.ps1
@@ -1,6 +1,6 @@
 $link="https://www.minv.sk/?odcudzene-mot-vozidla"
 
-$inp=@{VIN='TMVJG7BE4J0290743'}
+$inp=@{V1N='TMVJG7BE4J0290743'}
 
 $req=iwr -Method Post -Uri $link -Body $inp -ContentType "application/x-www-form-urlencoded"
 
