b2e8dec11a956fca696e7d79a271479f728dba71
spectator8 <86881057+spectator8@users.noreply.github.com>
2023-03-19 23:27:43 +0100

    Delete checkVehicleByVIN.ps1

--- checkVehicleByVIN.ps1
+++
@@ -1,8 +0,0 @@
-$link="https://www.minv.sk/?odcudzene-mot-vozidla"
-
-$inp=@{V1N='TMVJG7BE4J0290743'}
-
-$req=iwr -Method Post -Uri $link -Body $inp -ContentType "application/x-www-form-urlencoded"
-
-
-$resH=($req.parsedHtml.getElementsByClassName('tabulka4')[0].outerText) -split "`n" | Write-Host
